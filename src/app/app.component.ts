07ffa3ab7347f8556d8ee5389cb51592c92636a9
Álvaro Octal <alvaro.octal@gmail.com>
2025-10-07 19:19:52 +0200

    Implemented logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
+    standalone: true,
     templateUrl: './app.component.html',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule],
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    public id: string | undefined;
-    public originalURL: string;
+    protected originalURL: string;
+    protected isLoggedIn: boolean = false;
 
     private readonly router: Router = inject(Router);
     private readonly auth: Auth = inject(Auth);
@@ -20,14 +22,20 @@ export class AppComponent {
 
         this.auth.onAuthStateChanged(async (user): Promise<void> => {
             if (user) {
+                this.isLoggedIn = true;
                 if (this.originalURL.startsWith('/room')) {
                     await this.router.navigateByUrl(this.originalURL);
                 } else {
                     await this.router.navigateByUrl('/');
                 }
             } else {
+                this.isLoggedIn = false;
                 await this.router.navigateByUrl('/login');
             }
         });
+    }
+
+    protected async logout(): Promise<void> {
+        await this.auth.signOut();
     }
 }
